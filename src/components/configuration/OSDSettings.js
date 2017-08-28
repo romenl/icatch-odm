72edeaa94df9ae87162e61fb3e06a77a5bfb5cca
GeorgioWan <georgio.wan@icatchinc.com>
2017-08-28 16:39:21 +0800

    finish OSD settings

---
+++ src/components/configuration/OSDSettings.js
@@ -0,0 +1,256 @@
+import React, { Component } from 'react';
+import update from 'react/lib/update';
+import { Row, Col, Spin, Card , Form, Button, message } from 'antd';
+import { FormItemSelect, FormItemSlider, FormColorPicker, FormItemInputNumber } from '../CustomInput';
+
+// Onvif API
+import { 
+    GetOSDOptions,
+    GetOSDs,
+    SetOSD
+} from '../../onvif/';
+
+const FormItem = Form.Item;
+
+class OSDSettings extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            spin_tip: 'Loading ...',
+            isSpinning: true
+        };
+    }
+    async refreshInformation() {
+        try {
+            let data = await Promise.all([
+                GetOSDOptions(0),
+                GetOSDs(0)
+            ]);
+            
+            this.setState({
+                options: data[0],
+                settings: data[1][0],
+                isSpinning: false
+            });
+        } catch(e) {
+            console.log( '[ERROR] ', e );
+        }
+    }
+    componentDidMount(){
+        this.refreshInformation();
+    }
+    handleReset(){
+        const defaultSettings = {
+            background_color: {
+                color: {r: 0, g: 0, b: 0, a: 0.6}
+            },
+            dateformat: "yyyy/MM/dd",
+            font_color: {
+                color:{r: 255, g: 255, b: 255, a: 1}
+            },
+            font_size: 40,
+            position_type: "UpperLeft",
+            position_x: 0,
+            position_y: 0,
+            text_type:"DateAndTime",
+            timeformat:"HH:mm:ss"
+        };
+
+        this.deliver('Reset Default ...', 'Reset your configuration.', defaultSettings);
+
+        this.props.form.setFieldsValue(defaultSettings);
+        this.handlePositionChange();
+    }
+    handlePositionChange(){
+        const type = this.props.form.getFieldValue('position_type');
+        
+        if ( this.state.settings )
+            this.setState( update( this.state, {
+                settings: {
+                    Position: { 
+                        Type: { $set: type }
+                    }
+                }
+            }));
+        
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        this.deliver('Saving ...', 'Saved your configuration.');
+    }
+    deliver(tip, msg='Your configuration is changed.', reset){
+        this.props.form.validateFields( async (err, values) => {
+            if (!err) {
+                this.setState({
+                    spin_tip: tip,
+                    isSpinning: true
+                });
+                
+                await SetOSD( this.setSettings( reset, values ) );
+                
+                // Close Spinning
+                this.setState({ isSpinning: false });
+                
+                message.success(msg);
+            }
+            else
+                message.error(err);
+        });
+    }
+
+    setSettings( reset, values ){
+        let settings = this.state.settings.data;
+        let nextSettings = reset ? reset : values;
+        
+        // Position
+        settings.Position.Type.v = nextSettings.position_type;
+        settings.Position.Pos.x.v = nextSettings.position_x ? parseInt(nextSettings.position_x, 10) : 0;
+        settings.Position.Pos.y.v = nextSettings.position_y ? parseInt(nextSettings.position_y, 10) : 0;
+        // TextString
+        settings.TextString.Type.v = nextSettings.text_type;
+        settings.TextString.DateFormat.v = nextSettings.dateformat;
+        settings.TextString.TimeFormat.v = nextSettings.timeformat;
+        settings.TextString.FontSize.v = nextSettings.font_size;
+
+        settings.TextString.FontColor.Color.X.v = nextSettings.font_color.color.r;
+        settings.TextString.FontColor.Color.Y.v = nextSettings.font_color.color.g;
+        settings.TextString.FontColor.Color.Z.v = nextSettings.font_color.color.b;
+        settings.TextString.FontColor.Transparent.v = parseInt(nextSettings.font_color.color.a * 255, 10);
+
+        settings.TextString.BackgroundColor.Color.X.v = nextSettings.background_color.color.r;
+        settings.TextString.BackgroundColor.Color.Y.v = nextSettings.background_color.color.g;
+        settings.TextString.BackgroundColor.Color.Z.v = nextSettings.background_color.color.b;
+        settings.TextString.BackgroundColor.Transparent.v = parseInt(nextSettings.background_color.color.a * 255, 10);
+        
+        return settings;
+    }
+    render() {
+        const { spin_tip, isSpinning } = this.state;
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: { span: 8 },
+            wrapperCol: { span: 12 },
+        };
+        
+        const { options, settings } = this.state;
+
+        const fontColor = settings ? {
+            r: settings.TextString.FontColor.Color.X,
+            g: settings.TextString.FontColor.Color.Y,
+            b: settings.TextString.FontColor.Color.Z,
+            a: settings.TextString.FontColor.Transparent
+        } : {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: 100
+        };
+        const bgColor = settings ? {
+            r: settings.TextString.BackgroundColor.Color.X,
+            g: settings.TextString.BackgroundColor.Color.Y,
+            b: settings.TextString.BackgroundColor.Color.Z,
+            a: settings.TextString.BackgroundColor.Transparent
+        } : {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: 100
+        };
+
+        const costum_pos = (settings && settings.Position.Type === 'Custom') ? true : false;
+        
+        return (
+            <Spin tip={ spin_tip } spinning={ isSpinning }>
+                <h1>OSD Settings</h1>
+                <Form onSubmit={this.handleSubmit.bind(this)}>
+                    <Row gutter={16}>
+                        <Col span={8}>
+                            <Card title={<h3 style={{marginBottom: 24, textAlign: 'center'}}>Position</h3>}>
+                                <FormItemSelect 
+                                    label='Type' 
+                                    id='position_type' 
+                                    value={ settings ? settings.Position.Type : 'NaN' } 
+                                    options={ options ? options.PositionOption : [{value: 0, name: 'NaN'}] } 
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                    onChange={this.handlePositionChange.bind(this)}
+                                />                            
+                                <FormItemInputNumber
+                                    label='X' 
+                                    id='position_x' 
+                                    value={ settings ? settings.Position.Pos.x : 0 } 
+                                    disabled={!costum_pos}
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                    min={0}
+                                />
+                                <FormItemInputNumber
+                                    label='Y' 
+                                    id='position_y' 
+                                    value={ settings ? settings.Position.Pos.y : 0 } 
+                                    disabled={!costum_pos}
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                    min={0}
+                                />
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title={<h3 style={{marginBottom: 24, textAlign: 'center'}}>Format</h3>}>
+                                <FormItemSelect 
+                                    label='Text Type'
+                                    id='text_type' 
+                                    value={ settings ? settings.TextString.Type : 'NaN' } 
+                                    options={ options ? options.TextOption.Type : [{value: 0, name: 'NaN'}] } 
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                />
+                                <FormItemSelect 
+                                    label='Date Format' 
+                                    id='dateformat' 
+                                    value={ settings ? settings.TextString.DateFormat : 'NaN' } 
+                                    options={ options ? options.TextOption.DateFormat : [{value: 0, name: 'NaN'}] } 
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                />
+                                <FormItemSelect 
+                                    label='Time Format' 
+                                    id='timeformat' 
+                                    value={ settings ? settings.TextString.TimeFormat : 'NaN' } 
+                                    options={ options ? options.TextOption.TimeFormat : [{value: 0, name: 'NaN'}] } 
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                />
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title={<h3 style={{marginBottom: 24, textAlign: 'center'}}>Style</h3>}>
+                                <FormItemSlider 
+                                    label='Font Size'
+                                    id='font_size'
+                                    value={ settings ? settings.TextString.FontSize : 12 }
+                                    min={ options ? options.TextOption.FontSizeRange.Min : 12 }
+                                    max={ options ? options.TextOption.FontSizeRange.Max : 40 }
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                />
+                                <FormColorPicker
+                                    label='Font Color'
+                                    id='font_color'
+                                    value={ fontColor }
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                />
+                                <FormColorPicker
+                                    label='Background Color'
+                                    id='background_color'
+                                    value={ bgColor }
+                                    layout={formItemLayout} decorator={getFieldDecorator}
+                                />
+                            </Card>
+                        </Col>
+                    </Row>
+
+                    <FormItem className='submit' wrapperCol={{ span: 6, offset: 18 }} style={{marginTop: 20}}>
+                        <Button onClick={this.handleReset.bind(this)} style={{ marginRight: 10 }}>Default</Button>
+                        <Button type="primary" htmlType="submit">Save</Button>
+                    </FormItem>
+                </Form>
+            </Spin>
+        );
+    }
+}
+
+export default OSDSettings = Form.create()(OSDSettings);
--- src/onvif/binding/media_binding.js
+++ src/onvif/binding/media_binding.js
@@ -1,56 +1,49 @@
 import * as schemas from "../lib/onvif_schemas";
+import { curryCMD, getKey } from './utils';
 import onvifCMD from './onvifCMD';
 
-export async function GetVideoEncoderConfiguration( token ) {
-    try {
-        let res = await onvifCMD( 'media', 'GetVideoEncoderConfiguration', token );
-        let configuration = res.Configuration;
-        
-        return {
-            data: configuration,
-            Resolution: `${configuration.Resolution.Width.v}x${configuration.Resolution.Height.v}`,
-            FPS: configuration.RateControl.FrameRateLimit.v,
-            Bitrate: configuration.RateControl.BitrateLimit.v
-        };
+export const GetVideoEncoderConfiguration = 
+curryCMD( 'GetVideoEncoderConfiguration', async (arg) => {
+    let token = arg[0];
+    let res = await onvifCMD( 'media', 'GetVideoEncoderConfiguration', token );
+    let configuration = res.Configuration;
+    
+    return {
+        data: configuration,
+        Resolution: `${configuration.Resolution.Width.v}x${configuration.Resolution.Height.v}`,
+        FPS: configuration.RateControl.FrameRateLimit.v,
+        Bitrate: configuration.RateControl.BitrateLimit.v
+    };
+});
 
-    } catch(e) {
-        console.log("[GetVideoEncoderConfiguration] ", e);
-    }
-}
+export const GetVideoEncoderConfigurationOptions =
+ curryCMD( 'GetVideoEncoderConfigurationOptions', async (arg) => {
+    let token = arg[0];
+    let res = await onvifCMD( 'media', 'GetVideoEncoderConfigurationOptions', token );
+    let options = res.Options;
+    
+    let resolutionAvailable = options.H264.ResolutionsAvailable.v,
+        resoltionOptions = [];
 
-export async function GetVideoEncoderConfigurationOptions( token ) {
-    try {
-        let res = await onvifCMD( 'media', 'GetVideoEncoderConfigurationOptions', token );
-        let options = res.Options;
-        
-        let resolutionAvailable = options.H264.ResolutionsAvailable.v,
-            resoltionOptions = [];
-
-        resolutionAvailable.forEach((r, index) => {
-            resoltionOptions.push({
-                value: index,
-                name: `${r.Width.v}x${r.Height.v}`
-            });
+    resolutionAvailable.forEach((r, index) => {
+        resoltionOptions.push({
+            value: index,
+            name: `${r.Width.v}x${r.Height.v}`
         });
-        
-        return {
-            H264: {
-                Resolution: resoltionOptions,
-                FrameRateRange: {
-                    Min: options.H264.FrameRateRange.Min.v,
-                    Max: options.H264.FrameRateRange.Max.v
-                }
-            },
-            BitrateRange: {
-                Min: options.Extension.H264.BitrateRange.Min.v,
-                Max: options.Extension.H264.BitrateRange.Max.v
-            }
-        };
-        
-    } catch(e) {
-        console.log("[GetVideoEncoderConfiguration] ", e);
-    }
-}
+    });
+    
+    return {            
+        Resolution: resoltionOptions,
+        FrameRateRange: {
+            Min: options.H264.FrameRateRange.Min.v,
+            Max: options.H264.FrameRateRange.Max.v
+        },
+        BitrateRange: {
+            Min: options.Extension.H264.BitrateRange.Min.v,
+            Max: options.Extension.H264.BitrateRange.Max.v
+        }
+    };
+});
 
 /**
  * settings: { mainSettings, subSettings }
@@ -58,19 +51,142 @@ export async function GetVideoEncoderConfigurationOptions( token ) {
  * @export
  * @param {mainSettings, subSettings} settings
  */
-export async function SetVideoEncoderConfiguration( settings ) {
-    try {
-        let mainConfiguration = new schemas.trt_SetVideoEncoderConfiguration(),
-            subConfiguration  = new schemas.trt_SetVideoEncoderConfiguration();
-        
-        mainConfiguration.Configuration = settings.mainSettings;
-        subConfiguration.Configuration = settings.subSettings;
+export const SetVideoEncoderConfiguration = 
+curryCMD( 'SetVideoEncoderConfiguration', async (arg) => {
+    let settings = arg[0];
+    let mainConfiguration = new schemas.trt_SetVideoEncoderConfiguration(),
+        subConfiguration  = new schemas.trt_SetVideoEncoderConfiguration();
 
-        await Promise.all([
-            onvifCMD( 'media', 'SetVideoEncoderConfiguration', mainConfiguration ),
-            onvifCMD( 'media', 'SetVideoEncoderConfiguration', subConfiguration )
-        ]);
-    } catch(e) {
-        console.log("[SetVideoEncoderConfiguration] ", e);
-    }
-}
+    mainConfiguration.Configuration = settings.mainSettings;
+    subConfiguration.Configuration = settings.subSettings;
+
+    await Promise.all([
+        onvifCMD( 'media', 'SetVideoEncoderConfiguration', mainConfiguration ),
+        onvifCMD( 'media', 'SetVideoEncoderConfiguration', subConfiguration )
+    ]);
+});
+
+export const GetOSDOptions = 
+curryCMD( 'GetOSDOptions', async (arg) => {
+    let token = arg[0];
+    let res = await onvifCMD( 'media2', 'GetOSDOptions', token );
+    let options = res.OSDOptions;
+
+    const typeOptions = [{
+        value: '0',
+        name: 'Text'
+    },{
+        value: '1',
+        name: 'Image'
+    },{
+        value: '2',
+        name: 'Extended'
+    }];
+
+    let textType = options.TextOption.Type.v,
+        textTypeOptions = [];
+    textType.forEach((t, index) => {
+        textTypeOptions.push({
+            value: index,
+            name: `${t.v}`
+        });
+    });
+
+    let position = options.PositionOption.v,
+        positionOptions = [];
+    position.forEach((p, index) => {
+        positionOptions.push({
+            value: index,
+            name: `${p.v}`
+        });
+    });
+
+    let dateFormat = options.TextOption.DateFormat.v,
+        dateFormatOptions = [];
+    dateFormat.forEach((d, index) => {
+        dateFormatOptions.push({
+            value: index,
+            name: `${d.v}`
+        });
+    });
+
+    let timeFormat = options.TextOption.TimeFormat.v,
+        timeFormatOptions = [];
+    timeFormat.forEach((t, index) => {
+        timeFormatOptions.push({
+            value: index,
+            name: `${t.v}`
+        });
+    });
+
+    return {
+        Type: typeOptions,
+        PositionOption: positionOptions,
+        TextOption: {
+            Type: textTypeOptions,
+            FontSizeRange: {
+                Min: options.TextOption.FontSizeRange.Min.v,
+                Max: options.TextOption.FontSizeRange.Max.v
+            },
+            DateFormat: dateFormatOptions,
+            TimeFormat: timeFormatOptions
+        }
+    };
+});
+
+export const GetOSDs = 
+curryCMD( 'GetOSDs', async (arg) => {
+    let token = arg[0];
+    let res = await onvifCMD( 'media2', 'GetOSDs', token );
+    let osds = res.OSDs.v;
+    let datas = [];
+
+    osds.forEach(o => {
+        datas.push({
+            data: o,
+            Token: o.token.v,
+            Type: getKey( o.Type ),
+            Position: {
+                Type: o.Position.Type.v,
+                Pos: {
+                    x: o.Position.Pos.x.v,
+                    y: o.Position.Pos.y.v
+                }
+            },
+            TextString: {
+                Type: o.TextString.Type.v,
+                DateFormat: o.TextString.DateFormat.v,
+                TimeFormat: o.TextString.TimeFormat.v,
+                FontSize: o.TextString.FontSize.v,
+                FontColor: {
+                    Color: {
+                        X: o.TextString.FontColor.Color.X.v,
+                        Y: o.TextString.FontColor.Color.Y.v,
+                        Z: o.TextString.FontColor.Color.Z.v
+                    },
+                    Transparent: o.TextString.FontColor.Transparent.v / 255
+                },
+                BackgroundColor: {
+                    Color: {
+                        X: o.TextString.BackgroundColor.Color.X.v,
+                        Y: o.TextString.BackgroundColor.Color.Y.v,
+                        Z: o.TextString.BackgroundColor.Color.Z.v
+                    },
+                    Transparent: o.TextString.BackgroundColor.Transparent.v / 255
+                }
+            }
+        });
+    });
+     
+    return datas;
+});
+
+export const SetOSD = 
+curryCMD( 'SetOSD', async (arg) => {
+    let osd = arg[0];
+    let setOSD = new schemas.tr2_SetOSD();
+
+    setOSD.OSD = osd;
+    
+    await onvifCMD( 'media2', 'SetOSD', setOSD );
+});
